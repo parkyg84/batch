2366639e3bd38b0574841025593c177468f19952
parky <parkyg84@naver.com>
2022-05-23 22:37:00 +0900

    jdbc 설정

--- src/main/java/com/example/batch/batch/BatchConfiguration.java
+++ src/main/java/com/example/batch/batch/BatchConfiguration.java
@@ -1,20 +1,18 @@
 package com.example.batch.batch;
 
-
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -55,40 +53,41 @@ public class BatchConfiguration {
 
     @Bean
     public PersonItemProcessor processor() {
-
         return new PersonItemProcessor();
-
-    }
-
-
-
-    @Bean
-    public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
-        return new JdbcBatchItemWriterBuilder<Person>()
-                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
-                .sql("INSERT INTO monkey.people_new (first_name, last_name) VALUES (:firstName, :lastName)")
-                .dataSource(dataSource)
-                .build();
     }
 
     @Bean
-    public Job importUserJob(Step step1) {
-        return jobBuilderFactory.get("importUserJob")
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+        return jobBuilderFactory.get("importUserJob3")
                 .incrementer(new RunIdIncrementer())
-                //.listener(listener)
+                .listener(listener)
                 .flow(step1)
                 .end()
                 .build();
     }
 
     @Bean
-    public Step step1(JdbcBatchItemWriter<Person> writer) {
-        return stepBuilderFactory.get("step1")
-                .<Person, Person> chunk(10)
+    public Step step1() throws  Exception  {
+        return stepBuilderFactory.get("step3")
+                .<Person, Person> chunk(30)
                 .reader(reader())
                 .processor(processor())
-                .writer(writer)
+                .writer(writer())
                 .build();
+    }
+
+
+    public ItemWriter<Person> writer() throws  Exception{
+
+        JdbcBatchItemWriter<Person> itemWriter = new JdbcBatchItemWriterBuilder<Person>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .dataSource(dataSource)
+                .sql("INSERT INTO monkey.people2 (first_name, last_name) VALUES (:firstName, :lastName)")
+                .beanMapped()
+                .build();
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
     }
 
 
